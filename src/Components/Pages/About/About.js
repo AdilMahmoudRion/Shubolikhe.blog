9a717738b31ace557e43ac91940bc5633b052876
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-02-26 23:33:55 +0600

    about section added

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -3,6 +3,10 @@ import "./About.css";
 import author from "../../Shared/Images/author.jpg";
 import Navigation from "../../Shared/Navigation/Navigation";
 import Footer from "../../Shared/Footer/Footer";
+import facebook from "../../Shared/Images/social/facebook.png";
+import twitter from "../../Shared/Images/social/twitter.png";
+import linkedin from "../../Shared/Images/social/linkedin.png";
+import pinterest from "../../Shared/Images/social/pinterest.png";
 
 const About = () => {
   return (
@@ -13,10 +17,39 @@ const About = () => {
           <div className="author-image col-span-2">
             <img src={author} alt="" />
           </div>
-          <div className="author-information col-span-1">
-            <h1>About</h1>
+          <div className="author-information col-span-1 text-center">
+            <h1 className="text-5xl font-bold pb-5">About Me</h1>
             <div>
-              <h1>shubolikhe.blog</h1>
+              <p className="about-me">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Repudiandae, quasi iusto, provident eos eaque architecto libero
+                odit omnis esse, beatae cumque quo numquam harum nam facilis
+                voluptatem deserunt aliquid. Ab, sequi odit dolore iure,
+                mollitia corporis ea dignissimos eligendi facilis autem aliquam
+                voluptates suscipit, fugit reiciendis vel aut animi consequatur.
+                Dolor repellendus eius similique possimus perspiciatis numquam!
+                Necessitatibus nulla natus labore id tempore!
+              </p>
+            </div>
+
+            <div className="about-me-social-link-section">
+              <a href="#">
+                <img className="about-social-icon" src={twitter} alt="" />
+              </a>
+              <a href="#">
+                <img className="about-social-icon" src={facebook} alt="" />
+              </a>
+              <a href="">
+                <img
+                  className="about-social-icon"
+                  src={linkedin}
+                  alt=""
+                  srcset=""
+                />
+              </a>
+              <a href="">
+                <img className="about-social-icon" src={pinterest} alt="" />
+              </a>
             </div>
           </div>
         </div>
